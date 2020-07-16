f08e28d79011daa7b47fde00019559bf2d98874a
JHtaeng <68258591+JHtaeng@users.noreply.github.com>
2020-07-16 16:15:53 +0900

    bookmark app 개발

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'taggit.apps.TaggitAppConfig',
     'taggit_templatetags2',
     'photo.apps.PhotoConfig',
+    'widget_tweaks',
 
 ]
 
@@ -137,3 +138,6 @@ TAGGIT_LIMIT = 50
 
 DISQUS_SHORTNAME ='netteholic'
 DISQUS_MY_DOMAIN = 'http://127.0.0.1:8000'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
