88a8e78ff549d4bca8cb101e8f63e4515550f11a
JHtaeng <68258591+JHtaeng@users.noreply.github.com>
2020-07-17 13:39:24 +0900

    bookmark app 개발

--- ch99/blog/views.py
+++ ch99/blog/views.py
@@ -91,7 +91,7 @@ class SearchFormView(FormView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'slug', 'description', 'content', 'tags']
-    initial = {'slug','auto-filling-do-not-input'}
+    initial = {'slug':'auto-filling-do-not-input'}
     #fields = ['title','description','content','tags']
     success_url = reverse_lazy('blog:index')
 
--- ch99/bookmark/views.py
+++ ch99/bookmark/views.py
@@ -40,3 +40,5 @@ class BookmarkDeleteView(OwnerOnlyMixin, DeleteView):
 
 
 
+
+
